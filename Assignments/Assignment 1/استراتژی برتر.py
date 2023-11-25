0db3afae5841ebd67afc7ead565160795fa59b22
Imanm02 <imanm1381@gmail.com>
2023-11-25 19:47:12 +0330

    update Assignment 1
    Co-authored-by: Iman Mohammadi <imanm1381@gmail.com>
    Co-authored-by: Ali Salesi <ali_sal1381@yahoo.com>
    Co-authored-by: Donya Navabi <dnya.nvb@gmail.com>
    Co-authored-by: Arman Malekzadeh <malekzadeh@ieee.org>
    Co-authored-by: Hoora Abedin <hooraabedin2004@gmail.com>
    Co-authored-by: Abolfazl Sheikhha <abolfazlsheikhha.833@gmail.com>
    Co-authored-by: Parsa Ghezelbash <ghezelbash.parsa.ghezelbash83@gmail.com>
    Co-authored-by: Mohammad Arian Iravani <arian.iravaninia@gmail.com>
    Co-authored-by: Saeed Forati <Saeed.forati.k@gmail.com>

--- Assignments/Assignment 1/استراتژی برتر.py
+++ Assignments/Assignment 1/استراتژی برتر.py
@@ -1,14 +1,20 @@
-a = int(input())
-b = int(input())
-c = a ^ b
-
-bit = 1
-
-counter = 0
-
-for i in range(31):
-    if bit & c == bit:
-        counter += 1
-    bit = bit << 1
-
-print(counter)
+a, b = map(int, input().split())
+start = a
+end = b
+if a > b:
+    start = b
+    end = a
+count = 0
+for num in range(start, end+1):
+    is_prime = 1
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                is_prime = 0
+                break
+        if is_prime == 1:
+            count += 1
+if b >= a:
+    print("main order - amount: %d" % count)
+else:
+    print("reverse order - amount: %d" % count)
