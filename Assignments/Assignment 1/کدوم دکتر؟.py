eb18944a9fc7c70cef5bb471897950312e5248a5
Imanm02 <imanm1381@gmail.com>
2023-11-25 19:44:28 +0330

    update Assignment 1
    Co-authored-by: Iman Mohammadi <imanm1381@gmail.com>
    Co-authored-by: Ali Salesi <ali_sal1381@yahoo.com>
    Co-authored-by: Donya Navabi <dnya.nvb@gmail.com>
    Co-authored-by: Arman Malekzadeh <malekzadeh@ieee.org>
    Co-authored-by: Hoora Abedin <hooraabedin2004@gmail.com>
    Co-authored-by: Abolfazl Sheikhha <abolfazlsheikhha.833@gmail.com>
    Co-authored-by: Parsa Ghezelbash <ghezelbash.parsa.ghezelbash83@gmail.com>
    Co-authored-by: Mohammad Arian Iravani <arian.iravaninia@gmail.com>

--- Assignments/Assignment 1/کدوم دکتر؟.py
+++ Assignments/Assignment 1/کدوم دکتر؟.py
@@ -1,1 +1,13 @@
-print("Hello Python")
+a = int(input())
+b = int(input())
+c = a ^ b
+
+bit = 1
+
+counter = 0
+
+for i in range(31):
+    if bit & c == bit:
+        counter += 1
+    bit = bit << 1
+print(counter)
